85f6f106247a5a5e32fa61771eddf05aaa18ffe4
Jacob Ebey <jacob.ebey@live.com>
2022-09-14 19:14:01 -0700

    updated offline route

--- app/routes/service-worker/offline.tsx
+++ app/routes/service-worker/offline.tsx
@@ -1,3 +1,22 @@
-export default function Offline() {
-  return <h1>Offline</h1>;
+import { json } from "#remix-server";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  return json({
+    message: "This page was rendered in your browser via Service Workers.",
+  });
+}
+
+export default function Index() {
+  const { message } = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h1>Welcome to Remix!</h1>
+      <p>{message}</p>
+      <p>
+        Go back <Link to="/">Home</Link>.
+      </p>
+    </div>
+  );
 }

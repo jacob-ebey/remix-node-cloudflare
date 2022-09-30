d6742ae318a5a10f65ac502439c15929c9ed1f64
Jacob Ebey <jacob.ebey@live.com>
2022-09-29 17:07:17 -0700

    updated tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    setupFiles: "./tests/setup.ts",
     alias: {
       "#remix-server": "@remix-run/node",
     },
